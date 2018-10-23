c44a2e3ec6c1da4e17612f2af52adf450a0ba7cf
Rodrigo Saldanha Dutra <rodrigo.dutra@meta.com.br>
2018-10-23 13:46:51 -0300

    add pack json

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+ });
 
 app.get('/',function(req,res){
     console.log("teste");
+    res.end( "data" );
 });
