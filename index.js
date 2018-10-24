3187608c92ad8d49607410d5dd52d0e30fdb9fe6
Rodrigo Dutra <rodrigo.dutra92@gmail.com>
2018-10-24 12:46:59 -0300

    added modules and change to work in heroku

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var express = require("express");
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
 var app = express();
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
- });
+
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+// var server = app.listen(8081, function () {
+//     var host = server.address().address
+//     var port = server.address().port
+//     console.log("Example app listening at http://%s:%s", host, port)
+//  });
 
 app.get('/',function(req,res){
-    console.log("teste");
-    res.end( "data" );
+    console.log("test");
+    res.end( "data teste" );
 });
